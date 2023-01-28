3d14cf7874306df1291550b61d6d3be2c21f4e7a
mayurkwagh <123708179+mayurkwagh@users.noreply.github.com>
2023-01-28 14:29:09 +0530

    changes of project

--- Demo_project/src/my_classes_package/Login_page.java
+++ Demo_project/src/my_classes_package/Login_page.java
@@ -5,7 +5,7 @@ public class Login_page
 	 
 	public static void main(String[] args) {
 		
-		System.out.println("Hellow everyone my task is completed");
+		System.out.println("Hellow All everyone my task is completed");
 	}
 
 }
---
+++ Demo_project/src/my_classes_package/Logout_Page.java
@@ -0,0 +1,11 @@
+package my_classes_package;
+
+public class Logout_Page
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("logout successfully");
+		
+	}
+
+}
