10f0753d14483b11cdec654f8b9c5c6020a52aba
mayurkwagh <123708179+mayurkwagh@users.noreply.github.com>
2023-01-28 07:16:31 +0530

    Basic task

---
+++ Demo_project/src/my_classes_package/My_First_Class.java
@@ -0,0 +1,13 @@
+package my_classes_package;
+
+public class My_First_Class
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello everyone");
+		System.out.println("this is my first java project");
+		System.out.println("i have created an one demo project on github desktop");
+
+	}
+
+}
